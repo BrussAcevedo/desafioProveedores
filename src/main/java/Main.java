7f95d259cd7c3bd1d44f00ba71305ed6665eb2b8
Bruss Acevedo <bruss.acevedo@gmail.com>
2024-07-19 21:58:05 -0400

    "conexion y metodos CRUD solicitados"

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import cl.desafioLatam.DAO.ProveedorDAO;
+import cl.desafioLatam.DTO.ProveedorDTO;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//ProveedorDTO proveedor = new ProveedorDTO(0, "hola", "HOLA2", "HOLA2", "HOLA2", "HOLA2", "HOLA2", "HOLA2");
+		//ProveedorDAO proveedordao = new ProveedorDAO();
+		
+		//Integer result = proveedordao.insertProveedor(proveedor);
+		//System.out.println(result);
+		
+		
+		//List<ProveedorDTO> proveedores = new ArrayList<>();
+		//proveedores = proveedordao.listarProveedores();
+		//for (ProveedorDTO proveedor: proveedores) {
+		//	System.out.println(proveedor.getNombre());
+		//	System.out.println(proveedor.getId());
+		//}
+	}
+
+}
